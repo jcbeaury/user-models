f804e66ab03bf35ded2afd565d58831522e2be84
jcbeaury <jcbeaury@gmail.com>
2014-08-04 17:52:38 -0400

    Fix dumb typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  belongs to :user_type, polymorphic => true
+  belongs_to :user_type, :polymorphic => true
 end
