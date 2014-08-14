c938567e8aed427d5b8651e45c643cd7d263173b
jcbeaury <jcbeaury@gmail.com>
2014-08-14 12:28:03 -0400

    Add custom routes for each user type

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
 
   def index
+    if current_user.role.class == Admin
+      redirect_to '/admin/bikers'
+    elsif current_user.role.class == Biker
+      redirect_to '/bikers'
+    elsif current_user.role.class == Skier
+      redirect_to '/skiers'
+    end
   end
 
 end
